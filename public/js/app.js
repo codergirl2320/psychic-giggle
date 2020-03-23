df4c580ba7cdc3965f98861979ab2c9f9bada10f
Tania Diane Dodson <taniad0916@MacBook.local>
2020-03-23 18:19:29 -0400

    more functionality

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ const app = angular.module('RecipeApp', [])
 app.controller('MainController', ['$http', function($http){
   this.title = null
   this.url = null
+  this.indexOfEditFormToShow = null
   const controller = this
 
 this.createRecipe = function(){
@@ -15,12 +16,45 @@ this.createRecipe = function(){
     }
   }).then(
     function(response){
-      // controller.title = null
-      // controller.url = null
+      controller.title = null
+      controller.url = null
       controller.getRecipes()
     },
     function(err){
       console.log(err)
+    }
+  )
+}
+
+this.editRecipe = function(rcpe){
+  $http({
+    method: 'PUT',
+    url: '/recipes/' + rcpe._id,
+    data: {
+      title: this.updatedTitle,
+      url: this.updatedUrl
+    }
+    }).then(
+      function(response){
+        controller.indexOfEditFormToShow = null
+        controller.getRecipes()
+      },
+      function(err){
+        console.log(err)
+      }
+    )
+  }
+
+this.deleteRecipe = function(rcpe){
+  $http({
+    method: 'DELETE',
+    url: '/recipes/' + rcpe._id
+  }).then(
+    function(){
+      controller.getRecipes()
+    },
+    function(err){
+
     }
   )
 }
