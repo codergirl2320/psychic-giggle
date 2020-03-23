398c31e82d7340b6684c314bb4272b127a58496d
Tania Diane Dodson <taniad0916@MacBook.local>
2020-03-23 19:51:05 -0400

    changed code to make fewer calls, added search box, recipes are alphabetical

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ partner project
 
 Owner: Tania Dodson
 Collaborator: Steve Akinfenwa
+
+Tania Dodson:
+I created the repo and created the public folder files.
+
+
+Steve Akinfenwa:
+Cloned the repo and created the server.js file and the back-end controller and model.
--- public/js/app.js
+++ public/js/app.js
@@ -6,34 +6,34 @@ app.controller('MainController', ['$http', function($http){
   this.indexOfEditFormToShow = null
   const controller = this
 
-this.createRecipe = function(){
-  $http({
-    method: 'POST',
-    url: '/recipes',
-    data: {
-      title: this.title,
-      url: this.url
-    }
-  }).then(
-    function(response){
-      controller.title = null
-      controller.url = null
-      controller.getRecipes()
-    },
-    function(err){
-      console.log(err)
-    }
-  )
-}
+  this.createRecipe = function(){
+    $http({
+      method: 'POST',
+      url: '/recipes',
+      data: {
+        title: this.title,
+        url: this.url
+      }
+    }).then(
+      function(response){
+        controller.title = null
+        controller.url = null
+        controller.recipes.push(response.data)
+      },
+      function(err){
+        console.log(err)
+      }
+    )
+  }
 
-this.editRecipe = function(rcpe){
-  $http({
-    method: 'PUT',
-    url: '/recipes/' + rcpe._id,
-    data: {
-      title: this.updatedTitle,
-      url: this.updatedUrl
-    }
+  this.editRecipe = function(recipe){
+    $http({
+      method: 'PUT',
+      url: '/recipes/' + recipe._id,
+      data: {
+        title: this.updatedTitle,
+        url: this.updatedUrl
+      }
     }).then(
       function(response){
         controller.indexOfEditFormToShow = null
@@ -45,33 +45,34 @@ this.editRecipe = function(rcpe){
     )
   }
 
-this.deleteRecipe = function(rcpe){
-  $http({
-    method: 'DELETE',
-    url: '/recipes/' + rcpe._id
-  }).then(
-    function(){
-      controller.getRecipes()
-    },
-    function(err){
+  this.deleteRecipe = (id) => {
+    $http({
+      method: 'DELETE',
+      url: `/recipes/${id}`,
+    }).then ((response) => {
+      console.log(response.data)
+      const removeByIndex = controller.recipes.findIndex((recipe) => {
+        recipe._id === id
+      })
+        controller.recipes.splice(removeByIndex, 1)
+      }, (error) => {
+        console.log(error)
+    })
+  }
 
-    }
-  )
-}
-
-this.getRecipes = function(){
-  $http({
-    method: 'GET',
-    url: '/recipes'
-  }).then(
-    function(response){
-      console.log(controller)
-      controller.recipes = response.data
-    },
-    function(err){
-    }
-  )
-}
+  this.getRecipes = function(){
+    $http({
+      method: 'GET',
+      url: '/recipes'
+    }).then(
+      function(response){
+        console.log(controller)
+        controller.recipes = response.data
+      },
+      function(err){
+      }
+    )
+  }
 
 this.getRecipes()
 
